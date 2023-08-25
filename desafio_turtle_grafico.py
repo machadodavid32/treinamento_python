f5a7648a2b04944324f726b7f415d03e95e76a92
David Machado <machadodavid32@gmail.com>
2023-08-25 08:05:41 -0300

    Projeto grafico via turtle em andamento

--- desafio_turtle_grafico.py
+++ desafio_turtle_grafico.py
@@ -15,16 +15,30 @@ while True:
     direita = 0
     esquerda = 0
     tras = 0
-    direção = input('O que quer fazer?:  ')
+    direcao = input('O que quer fazer?: di = distancia, d = rotação a direita, e = rotacao a esquerda, t = ir para tras ')
     
-    distancia = int(input('Informe a distância: '))
-    t.forward(distancia)
+    if direcao == 'di':
+        distancia = int(input('Informe a distância: '))
+        t.forward(distancia)
     
-    direita = int(input('Informe rotação á direita: '))
-    t.right(direita)
+    elif direcao == 'd':    
+        direita = int(input('Informe rotação á direita: '))
+        t.right(direita)
     
-    esquerda = int(input('Informe rotação á esquerda: '))
-    t.left(esquerda)
+    elif direcao == 'e':
+        esquerda = int(input('Informe rotação á esquerda: '))
+        t.left(esquerda)
+    
+    elif direcao == 't':
+        tras = int(input('Informe quantos pixels para tras: '))
+        t.backward(tras)    
+    else:
+        print('Até mais')
+        break
+    
+    
+    
+    
     
     #c = int(input('Informe quantos píxels para cima: '))
     #t.up()
@@ -32,7 +46,4 @@ while True:
     #b = int(input('Informe quantos píxels para baixo: '))
     #t.down()
     
-    traz = int(input('Informe quantos pixels para tras: '))
-    t.backward(traz)
     
-    input()
